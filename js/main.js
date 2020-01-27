a2191290a09c8c11aae9f96831c45f769b24274e
Jason Cameron <jasoncameron246@gmail.com>
2020-01-27 05:40:47 -0500

    contact & footer added

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//Init and add map
+
+function initMap() {
+  // Your Location
+  const loc = { lat: 42.361145, lng: -71.057083 }
+  // centered map on location
+  const map = new google.maps.Map(document.querySelector('.map'),
+    {
+      zoom: 14,
+      center: loc
+    }
+  )
+  // The marker, positioned at location
+
+  const marker = new google.maps.Marker({ position: loc, map: map })
+}
