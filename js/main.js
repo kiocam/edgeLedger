da7fbb4628ca465c81c89755c77a675b69747578
Jason Cameron <jasoncameron246@gmail.com>
2020-05-15 14:24:51 -0400

    project refactor-header section

--- js/main.js
+++ js/main.js
@@ -1,42 +1,42 @@
-// Init and add map
+// // Init and add map
 
-function initMap() {
-  // Your Location
-  const loc = { lat: 42.361145, lng: -71.057083 }
-  // centered map on location
-  const map = new google.maps.Map(document.querySelector('.map'),
-    {
-      zoom: 14,
-      center: loc
-    }
-  )
-  // The marker, positioned at location
+// function initMap() {
+//   // Your Location
+//   const loc = { lat: 42.361145, lng: -71.057083 }
+//   // centered map on location
+//   const map = new google.maps.Map(document.querySelector('.map'),
+//     {
+//       zoom: 14,
+//       center: loc
+//     }
+//   )
+//   // The marker, positioned at location
 
-  const marker = new google.maps.Marker({ position: loc, map: map })
-}
+//   const marker = new google.maps.Marker({ position: loc, map: map })
+// }
 
-// Sticky menu Background
+// // Sticky menu Background
 
-window.addEventListener('scroll', function () {
-  if (window.scrollY > 150) {
-    document.querySelector('#navbar').style.opacity = 0.7
-  } else {
-    document.querySelector('#navbar').style.opacity = 1
-  }
-})
+// window.addEventListener('scroll', function () {
+//   if (window.scrollY > 150) {
+//     document.querySelector('#navbar').style.opacity = 0.7
+//   } else {
+//     document.querySelector('#navbar').style.opacity = 1
+//   }
+// })
 
-// Smooth Scrolling
+// // Smooth Scrolling
 
-$('#navbar a, .btn').on('click', function (event) {
-  if (this.hash !== '') {
-    event.preventDefault()
+// $('#navbar a, .btn').on('click', function (event) {
+//   if (this.hash !== '') {
+//     event.preventDefault()
 
-    const hash = this.hash
-    $('html, body').animate(
-      {
-        scrollTop: $(hash).offset().top - 100
-      },
-      800
-    )
-  }
-})
+//     const hash = this.hash
+//     $('html, body').animate(
+//       {
+//         scrollTop: $(hash).offset().top - 100
+//       },
+//       800
+//     )
+//   }
+// })
